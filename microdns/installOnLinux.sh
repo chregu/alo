521f3561891b00b539409e05b1b298c29884a24d
Christian Stocker <me@chregu.tv>
2011-04-26 10:30:40 +0200

    added info what to do

--- microdns/installOnLinux.sh
+++ microdns/installOnLinux.sh
@@ -6,3 +6,5 @@ mv a.out /usr/local/bin/microdns
 chmod a+x /usr/local/bin/microdns*
 chmod a+x /etc/init.d/microdns
 update-rc.d microdns defaults 99
+echo "start with"
+echo "/etc/init.d/microdns start"
